8905288f6867f973657fc96aa18ef9ce014d3fa2
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-06 16:52:10 -0700

    Initial customizations
    Mostly fsharp setup

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -200,6 +200,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  desc = 'Color scheme customization',
+  group = vim.api.nvim_create_augroup('calum-color-scheme', { clear = true }),
+  callback = function()
+    -- These are mostly based on the fsharp lsp/treesitter defaults
+    vim.api.nvim_set_hl(0, '@lsp.type.module', { link = '@lsp.type.class' })
+    vim.api.nvim_set_hl(0, '@lsp.type.variable', { link = '@variable' })
+    vim.api.nvim_set_hl(0, '@keyword.import', { link = '@keyword' })
   end,
 })
 
@@ -629,6 +640,7 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        fsautocomplete = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -661,6 +673,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'fantomas', -- F# formatting
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -707,12 +720,13 @@ require('lazy').setup({
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 500,
+          timeout_ms = 5000,
           lsp_format = lsp_format_opt,
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        fsharp = { 'fantomas' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -920,6 +934,7 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    incremental_selection = { enable = true },
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -934,15 +949,15 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
