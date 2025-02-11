aeef6ea0ce2df704178fcfb498ab5d176fd9edd5
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-11 14:36:45 -0700

    Commenting improvements

--- after/ftplugin/fsharp.lua
+++ after/ftplugin/fsharp.lua
@@ -3,5 +3,4 @@ vim.opt_local.fdm = 'syntax'
 
 -- Comment settings
 vim.opt_local.formatoptions = 'croql'
-vim.opt_local.commentstring = '(*%s*)'
-vim.opt_local.comments = 's0:*,m0:*,ex0:*),s1:(*,mb:*,ex:*),:///:,://'
+vim.opt_local.comments = 's0:*,m0:*,ex0:*),s1:(*,mb:*,ex:*),:///,://'
