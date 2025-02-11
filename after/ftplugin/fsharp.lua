3dd0a0b9284cae7c1cde7a330526154213ea2ffa
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-10 17:43:45 -0700

    FSharp settings

---
+++ after/ftplugin/fsharp.lua
@@ -0,0 +1,7 @@
+-- enable syntax based folding
+vim.opt_local.fdm = 'syntax'
+
+-- Comment settings
+vim.opt_local.formatoptions = 'croql'
+vim.opt_local.commentstring = '(*%s*)'
+vim.opt_local.comments = 's0:*,m0:*,ex0:*),s1:(*,mb:*,ex:*),:///:,://'
