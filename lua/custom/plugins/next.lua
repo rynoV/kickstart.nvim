5118b98a98ed58d85e1471a16391c5231fd6657d
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-12 15:39:58 -0700

    nvim-next f and t mappings

--- lua/custom/plugins/next.lua
+++ lua/custom/plugins/next.lua
@@ -1,8 +1,15 @@
 return {
   'ghostbuster91/nvim-next',
-  opts = {
-    default_mappings = {
-      repeat_style = 'original',
-    },
-  },
+  opts = function()
+    local nvim_next_builtins = require 'nvim-next.builtins'
+    return {
+      default_mappings = {
+        repeat_style = 'original',
+      },
+      items = {
+        nvim_next_builtins.f,
+        nvim_next_builtins.t,
+      },
+    }
+  end,
 }
