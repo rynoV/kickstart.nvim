0b51922b59eb4eff9886f8a7ac5b2fd6daf7875f
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-12 19:09:27 -0700

    next conflict repeat in diffview

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -2,7 +2,26 @@ return {
   'NeogitOrg/neogit',
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
+    {
+      'sindrets/diffview.nvim',
+      opts = function()
+        local next_move = require 'nvim-next.move'
+        local actions = require 'diffview.actions'
+        local prev_conflict, next_conflict = next_move.make_repeatable_pair(actions.prev_conflict, actions.next_conflict)
+        return {
+          keymaps = {
+            view = {
+              { 'n', '[x', prev_conflict, { desc = 'In the merge-tool: jump to the previous conflict' } },
+              { 'n', ']x', next_conflict, { desc = 'In the merge-tool: jump to the next conflict' } },
+            },
+            file_panel = {
+              { 'n', '[x', prev_conflict, { desc = 'Go to the previous conflict' } },
+              { 'n', ']x', next_conflict, { desc = 'Go to the next conflict' } },
+            },
+          },
+        }
+      end,
+    }, -- optional - Diff integration
 
     -- Only one of these is needed.
     'nvim-telescope/telescope.nvim', -- optional
