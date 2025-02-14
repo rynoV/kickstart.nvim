4349e872ddf76233c55e64b54d1ebee9f09c0347
Calum Sieppert <sieppertcalum@gmail.com>
2025-02-13 17:35:30 -0700

    f# textobjects

--- queries/fsharp/textobjects.scm
+++ queries/fsharp/textobjects.scm
@@ -1,7 +1,16 @@
 (declaration_expression
    (function_or_value_defn
-        (function_declaration_left)
-        body: (_) @function.inner)
+      (function_declaration_left)
+      body: (_) @function.inner)
+   ) @function.outer
+
+; A workaround for functions with record destructuring arguments not recognized as functions
+(declaration_expression
+   (function_or_value_defn
+      (value_declaration_left
+        (identifier_pattern
+          (record_pattern)))
+      body: (_) @function.inner)
    ) @function.outer
 
 (member_defn
