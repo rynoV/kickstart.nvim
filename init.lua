9910f47bf1f462e98e6d19ff18f627a2c0489077
Calum Sieppert <sieppertcalum@gmail.com>
2025-03-13 10:23:33 -0600

    Use nord colour scheme

--- init.lua
+++ init.lua
@@ -235,6 +235,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     vim.api.nvim_set_hl(0, '@lsp.type.variable', { link = '@variable' })
     vim.api.nvim_set_hl(0, '@keyword.import', { link = '@keyword' })
     vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', { underdotted = true })
+    vim.api.nvim_set_hl(0, 'TabLineFill', {})
   end,
 })
 
@@ -1049,15 +1050,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'shaunsingh/nord.nvim',
+    'gbprod/nord.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.g.nord_borders = true
-      vim.g.nord_disable_background = true
-      vim.g.nord_italic = false
-      vim.g.nord_uniform_diff_background = false
-      vim.g.nord_bold = true
-      -- vim.cmd.colorscheme 'nord'
+      require('nord').setup {
+        transparent = true,
+      }
+      vim.cmd.colorscheme 'nord'
     end,
   },
 
@@ -1069,7 +1068,7 @@ require('lazy').setup({
       require('tokyonight').setup {
         transparent = true,
       }
-      vim.cmd.colorscheme 'tokyonight-storm'
+      -- vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
