92962106e17884812d4a4b62277dd904777dbbae
Calum Sieppert <sieppertcalum@gmail.com>
2025-03-20 21:04:42 -0600

    Update nord theme diff highlighting

--- init.lua
+++ init.lua
@@ -1062,6 +1062,18 @@ require('lazy').setup({
     init = function()
       require('nord').setup {
         transparent = true,
+        on_highlights = function(highlights, colors)
+          -- The default highlighting for diffs overrides the foreground syntax
+          -- highlighting. This changes it to an approach more like tokyo-night
+          -- which marks the diff regions using only the background colors
+          highlights.DiffAdd = { bg = colors.polar_night.brightest }
+          -- Deleted text is just the deletion markers and not any content, so
+          -- we set the foreground
+          highlights.DiffDelete = { fg = colors.aurora.red }
+          -- DiffText sits on top of DiffChange, so it should stand out
+          highlights.DiffChange = { bg = colors.polar_night.bright }
+          highlights.DiffText = { bg = colors.polar_night.brightest }
+        end,
       }
       vim.cmd.colorscheme 'nord'
     end,
