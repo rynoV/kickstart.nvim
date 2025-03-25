4e719355bf2e1429908f8f5013b71587019fd6ca
Calum Sieppert <sieppertcalum@gmail.com>
2025-03-25 11:43:06 -0600

    Fix theme of telescope fuzzy buffer search binding

--- init.lua
+++ init.lua
@@ -558,7 +558,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
           previewer = true,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
