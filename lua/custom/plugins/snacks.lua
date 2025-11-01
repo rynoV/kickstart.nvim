c45e03a63e2b9585c8facb2d144fef11bd67d2c2
Calum Sieppert <sieppertcalum@gmail.com>
2025-11-01 16:50:57 -0600

    snacks gh setup

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -26,6 +26,34 @@ return {
   },
   keys = {
     {
+      '<leader>gi',
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        Snacks.picker.gh_issue { state = 'all' }
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
+    },
+    {
       '<leader>ts',
       function()
         ---@diagnostic disable-next-line: missing-fields
