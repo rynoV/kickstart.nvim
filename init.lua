919f66a326fce3e0bd718e77faec29c020bf1f02
Calum Sieppert <sieppertcalum@gmail.com>
2026-04-12 21:46:55 -0600

    Some stuff from folke's config

--- init.lua
+++ init.lua
@@ -1,4 +1,35 @@
+if vim.loader then
+  -- Seems to take about 10ms off startup at time of writing
+  vim.loader.enable()
+end
+
 require('vim._core.ui2').enable()
+
+-- Setup some globals for debugging (lazy-loaded)
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+-- Override print to use snacks for `:=` command
+---@diagnostic disable-next-line: duplicate-set-field
+vim._print = function(_, ...)
+  dd(...)
+end
+
+if vim.env.PROF then
+  -- Assumes lazy.nvim
+  local snacks = vim.fn.stdpath 'data' .. '/lazy/snacks.nvim'
+  vim.opt.rtp:append(snacks)
+  require('snacks.profiler').startup {
+    startup = {
+      event = 'VimEnter', -- stop profiler on this event. Defaults to `VimEnter`
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  }
+end
 
 -- Allow local config files per-directory. :h 'exrc'
 vim.opt.exrc = true
