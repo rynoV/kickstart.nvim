3d0f6d54eb65ece91a2f067528bca67a56eb7345
Calum Sieppert <sieppertcalum@gmail.com>
2026-04-19 23:14:50 -0600

    Move options into their own file

--- init.lua
+++ init.lua
@@ -35,106 +35,7 @@ if vim.env.PROF then
   }
 end
 
--- Allow local config files per-directory. :h 'exrc'
-vim.opt.exrc = true
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
-vim.g.health = { style = 'float' }
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
---
--- On wsl, my solution for the clipboard was to install win32yank in windows (eg with winget). Also needed to ensure xclip wasn't installed on the linux distro.
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- Folding is enabled by default, but no folds are closed. Use zM to get a file
--- outline, or za to fold at the cursor, and zi to toggle folding.
-vim.opt.foldenable = true
-vim.opt.foldlevel = 9999
--- Use syntax highlighting for showing the first line of a closed fold
--- https://github.com/neovim/neovim/pull/20750
-vim.opt.foldtext = ''
-vim.opt.fillchars = 'fold: '
-
--- Show a border for floating windows
-vim.opt.winborder = 'rounded'
-
--- Behaviour I'm used to when closing tabs
-vim.opt.tabclose = { 'uselast', 'left' }
-
-vim.opt.completeopt:append { 'noinsert', 'popup' }
-
--- Ignore leading '-' for numbers when using C-a or C-x
-vim.opt.nrformats:append { 'unsigned' }
-
-vim.opt.conceallevel = 2
+require 'config.options'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -289,12 +190,6 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 vim.keymap.set('n', '<leader>ml', '<cmd>Lazy<cr>', { desc = 'Plugins' })
-
-vim.lsp.codelens.enable()
-vim.lsp.on_type_formatting.enable()
-
--- Lint treesitter queries :help ft-query-plugin
-vim.g.query_lint_on = { 'InsertLeave', 'TextChanged' }
 
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
@@ -669,6 +564,12 @@ require('lazy').setup({
       lazy = '💤 ',
     },
   },
+  performance = {
+    -- This allows using vim.cmd.packadd to enable optional built-in plugins.
+    -- It seems to add a few milliseconds to startup in some cases but not
+    -- significant
+    rtp = { reset = false },
+  },
 })
 
 local util = require 'custom.util'
@@ -704,8 +605,6 @@ Snacks.toggle
 --- current line, remembering the previous config when toggling it back on.
 ---@type vim.diagnostic.Opts.VirtualLines
 local virtual_lines_conf = { current_line = false }
-
-vim.diagnostic.config { virtual_lines = false }
 
 Snacks.toggle
   .new({
@@ -786,9 +685,6 @@ Snacks.toggle
   })
   :map '<leader>tl'
 
--- For some reason this packadd needs to come near the end of the file or it
--- doesn't load the plugin
-vim.cmd 'packadd! nvim.undotree'
 vim.keymap.set('n', '<leader>u', function()
   require('undotree').open {
     command = math.floor(vim.api.nvim_win_get_width(0) / 3) .. 'vnew',
@@ -806,17 +702,6 @@ end, { desc = '[U]ndotree toggle' })
 -- algorithm:minimal/patience/histogram.
 -- If the above don't work, you can use anchors to manually line up the
 -- regions.
-
-vim.opt.diffopt:remove 'inline:char'
-
-vim.opt.diffopt:append {
-  -- Using the new directory nvim.difftool with anchors breaks things if
-  -- hiddenoff is not set
-  'hiddenoff',
-  'vertical',
-  'inline:word',
-  'anchor',
-}
 
 local function make_diffopt_toggle(opt_name, key)
   Snacks.toggle
@@ -856,8 +741,6 @@ vim.api.nvim_create_user_command('DiffAnchor', function(opts)
 end, { nargs = 1, desc = 'Add a diff anchor' })
 
 vim.keymap.set('n', '<leader>DA', ':DiffAnchor ', { desc = 'Add a diff anchor at the mark' })
-
-vim.cmd 'packadd! nvim.difftool'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/config/options.lua
@@ -0,0 +1,116 @@
+-- See `:help vim.opt`
+-- For more options, you can see `:help option-list`
+
+-- Allow local config files per-directory. :h 'exrc'
+vim.opt.exrc = true
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+vim.g.health = { style = 'float' }
+
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+--
+-- On wsl, my solution for the clipboard was to install win32yank in windows (eg with winget). Also needed to ensure xclip wasn't installed on the linux distro.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Folding is enabled by default, but no folds are closed. Use zM to get a file
+-- outline, or za to fold at the cursor, and zi to toggle folding.
+vim.opt.foldenable = true
+vim.opt.foldlevel = 9999
+-- Use syntax highlighting for showing the first line of a closed fold
+-- https://github.com/neovim/neovim/pull/20750
+vim.opt.foldtext = ''
+vim.opt.fillchars = 'fold: '
+
+-- Show a border for floating windows
+vim.opt.winborder = 'rounded'
+
+-- Behaviour I'm used to when closing tabs
+vim.opt.tabclose = { 'uselast', 'left' }
+
+vim.opt.completeopt:append { 'noinsert', 'popup' }
+
+-- Ignore leading '-' for numbers when using C-a or C-x
+vim.opt.nrformats:append { 'unsigned' }
+
+vim.opt.conceallevel = 2
+
+-- Lint treesitter queries :help ft-query-plugin
+vim.g.query_lint_on = { 'InsertLeave', 'TextChanged' }
+
+vim.lsp.codelens.enable()
+vim.lsp.on_type_formatting.enable()
+vim.diagnostic.config { virtual_lines = false }
+
+vim.opt.diffopt:remove 'inline:char'
+
+vim.opt.diffopt:append {
+  -- Using the new nvim.difftool with directories and anchors breaks things if
+  -- hiddenoff is not set
+  'hiddenoff',
+  'vertical',
+  'inline:word',
+  'anchor',
+}
+
+vim.cmd.packadd { 'nvim.difftool', bang = not vim.v.vim_did_init }
+vim.cmd.packadd { 'nvim.undotree', bang = not vim.v.vim_did_init }
