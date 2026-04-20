84830c7ba9d097809d493560b5a4e0fd55bd59cb
Calum Sieppert <sieppertcalum@gmail.com>
2026-04-20 15:18:44 -0600

    silence tab maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -98,8 +98,8 @@ local util = require 'calum.util'
 -- Override of the default binding that moves to the next tab if the last
 -- accessed tab page no longer exists. I don't think the original binding is
 -- necessary, but it is still accessible under CTRL-W g<Tab>
-vim.keymap.set('n', 'g<Tab>', util.last_tab_or_next, { desc = 'Open last accessed tab or next tab' })
-vim.keymap.set('n', '<C-w>Q', ':tabclose<CR>', { desc = 'Tab close' })
+vim.keymap.set('n', 'g<Tab>', util.last_tab_or_next, { desc = 'Open last accessed tab or next tab', silent = true })
+vim.keymap.set('n', '<C-w>Q', ':tabclose<CR>', { desc = 'Tab close', silent = true })
 
 vim.keymap.set('n', '<leader>mf', util.open_file_in_last_tab, { desc = 'Open file at cursor in previously accessed tab page' })
 
